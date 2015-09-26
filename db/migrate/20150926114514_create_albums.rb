23762c0eeb7ef9437b27aa59d904be7471ecf5b4
MapuH <m.s.marinov@gmail.com>
2015-09-26 14:53:23 +0300

    created Album and Track models

---
+++ db/migrate/20150926114514_create_albums.rb
@@ -0,0 +1,17 @@
+class CreateAlbums < ActiveRecord::Migration
+  def change
+    create_table :albums do |t|
+      t.string :title
+      t.string :artist
+      t.string :year
+      t.integer :cd_number
+      t.string :genre
+      t.integer :tracks
+      t.string :label
+      t.string :image
+      t.string :listen
+
+      t.timestamps null: false
+    end
+  end
+end
