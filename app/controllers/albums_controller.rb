818728f40d49aa16b61a56536d9c17e323c65e57
MapuH <m.s.marinov@gmail.com>
2015-09-30 23:25:34 +0300

    create edit and update methods for albums controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -7,4 +7,22 @@ class AlbumsController < ApplicationController
   	@album = Album.find(params[:id])
   	@tracks = @album.tracks
   end
+
+  def edit
+  	@album = Album.find(params[:id])
+  end
+
+  def update
+  	@album = Album.find(params[:id])
+  	if @album.update_attributes(album_params)
+  		redirect_to(:action => "show", :id => @album.id)
+  	else
+  		render "edit"
+  	end
+  end
+
+  private
+  def album_params
+  	params.require(:album).permit(:title, :artist, :year, :cd_number, :genre, :tracks_num, :label, :image, :listen)
+  end
 end
